1d5b3198c6872686eeea2c49f43b3539e3ee2006
kebtech <64436886+kebtech@users.noreply.github.com>
2023-09-18 12:52:12 -0400

    added redis functions

--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,30 @@
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
-}
+use redis::{Connection, Commands};
 
-#[cfg(test)]
-mod tests {
-    use super::*;
+pub struct RedisStore {}
 
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+impl RedisStore {
+    pub fn connect() -> Result<Connection, anyhow::Error> {
+        let client = redis::Client::open("redis://127.0.0.1/")?;
+        let con = client.get_connection()?;
+        Ok(con)
     }
+
+    pub fn set(key: &str, value: &str) -> Result<(), anyhow::Error> {
+        let mut connection = Self::connect()?;
+        connection.set(key, value)?;
+        Ok(())
+    }
+
+    pub fn get(key: &str) -> Result<String, anyhow::Error> {
+        let mut connection = Self::connect()?;
+        let value = connection.get(key)?;
+        Ok(value)
+    }
+
+    pub fn delete(key: &str) -> Result<(), anyhow::Error> {
+        let mut connection = Self::connect()?;
+        connection.del(key)?;
+        Ok(())
+    }
+    
 }
