5a2ef6791e9c8d2268744e51b178b9ad39c4393f
kebtech <kyle@kebtech.com>
2024-04-06 02:35:51 -0400

    added a bunch of methods

--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,15 @@
 use redis::{Client, RedisResult};
-use r2d2_redis::RedisConnectionManager;
+use r2d2_redis::{redis::Commands, RedisConnectionManager};
 use r2d2::{Pool, PooledConnection}; 
+use serde::{de::DeserializeOwned, Serialize};
 use state::InitCell;
-pub struct Redis;
+pub struct CnctdRedis;
 
 type RedisPool = Pool<RedisConnectionManager>;
 
 static REDIS_POOL: InitCell<Option<RedisPool>> = InitCell::new();
 
-impl Redis {
+impl CnctdRedis {
     pub fn start_pool(redis_url: &str) -> anyhow::Result<RedisPool> {
         let manager = RedisConnectionManager::new(redis_url)?;
         let pool = Pool::builder().build(manager)?;
@@ -17,15 +18,43 @@ impl Redis {
         Ok(pool)
     }
 
-    pub fn get_pool() -> anyhow::Result<RedisPool> {
-        REDIS_POOL.get().clone().ok_or_else(|| anyhow::anyhow!("Redis pool not initialized"))
-    }
-
     pub fn get_client() -> anyhow::Result<PooledConnection<RedisConnectionManager>> {
-        let pool = Self::get_pool()?;
-        let conn = pool.get()?;
-
+        let pool = REDIS_POOL.get().clone().ok_or_else(|| anyhow::anyhow!("Redis pool not initialized"))?;
+        let conn = pool.get().map_err(|e| anyhow::anyhow!(e))?;
 
         Ok(conn)
     }
+
+    pub fn set<S>(key: &str, value: S) -> anyhow::Result<()> 
+    where S: Serialize + std::fmt::Debug + DeserializeOwned + Send + Sync + Clone + 'static{
+        let mut client = Self::get_client()?;
+        let value = serde_json::to_string(&value)?;
+
+        client.set(key, value)?;
+
+        Ok(())
+    }
+
+    pub fn get<V>(key: &str) -> anyhow::Result<V>
+    where
+        V: DeserializeOwned + std::fmt::Debug + Send + Sync + Clone + 'static,
+    {
+        let mut client = Self::get_client()?;
+    
+        let value_str: String = client.get(key)?;
+        let value: V = serde_json::from_str(&value_str)?;
+    
+        Ok(value)
+    }
+
+    pub fn publish<S>(channel: &str, message: S) -> anyhow::Result<()>
+    where S: Serialize + std::fmt::Debug + DeserializeOwned + Send + Sync + Clone + 'static {
+        let mut client = Self::get_client()?;
+        let message = serde_json::to_string(&message)?;
+
+        client.publish(channel, message)?;
+
+        Ok(())
+    }
+
 }
