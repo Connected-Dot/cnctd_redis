d384109ee9bae74e9fc223eec851213543dcc998
kebtech <kyle@kebtech.com>
2025-12-28 17:54:58 -0500

    fix after updated dependencies

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,6 @@
-use redis::{Client, ConnectionLike, RedisResult};
+use r2d2::{Pool, PooledConnection};
 use r2d2_redis::{redis::Commands, RedisConnectionManager};
-use r2d2::{Pool, PooledConnection}; 
+use redis::{Client, ConnectionLike, RedisResult};
 use serde::{de::DeserializeOwned, Serialize};
 use state::InitCell;
 pub struct CnctdRedis;
@@ -15,7 +15,7 @@ impl CnctdRedis {
         let pool = Pool::builder().build(manager)?;
         Self::check_connection(redis_url)?;
 
-        REDIS_POOL.set(pool); 
+        REDIS_POOL.set(pool);
 
         Ok(())
     }
@@ -27,12 +27,14 @@ impl CnctdRedis {
         Ok(conn)
     }
 
-    pub fn set<S>(key: &str, value: S) -> anyhow::Result<()> 
-    where S: Serialize + std::fmt::Debug + DeserializeOwned + Send + Sync + Clone + 'static{
+    pub fn set<S>(key: &str, value: S) -> anyhow::Result<()>
+    where
+        S: Serialize + std::fmt::Debug + DeserializeOwned + Send + Sync + Clone + 'static,
+    {
         let mut client = Self::get_pool()?;
         let value = serde_json::to_string(&value)?;
 
-        client.set(key, value)?;
+        let _: () = client.set(key, value)?;
 
         Ok(())
     }
@@ -42,30 +44,34 @@ impl CnctdRedis {
         V: DeserializeOwned + std::fmt::Debug + Send + Sync + Clone + 'static,
     {
         let mut client = Self::get_pool()?;
-    
+
         let value_str: String = client.get(key)?;
         let value: V = serde_json::from_str(&value_str)?;
-    
+
         Ok(value)
     }
 
     pub fn publish<S>(channel: &str, message: S) -> anyhow::Result<()>
-    where S: Serialize + std::fmt::Debug + DeserializeOwned + Send + Sync + Clone + 'static {
+    where
+        S: Serialize + std::fmt::Debug + DeserializeOwned + Send + Sync + Clone + 'static,
+    {
         let mut client = Self::get_pool()?;
         let message = serde_json::to_string(&message)?;
 
-        client.publish(channel, message)?;
+        let _: () = client.publish(channel, message)?;
 
         Ok(())
     }
 
     pub fn hset<S>(key: &str, field: &str, value: S) -> anyhow::Result<()>
-    where S: Serialize + std::fmt::Debug + DeserializeOwned + Send + Sync + Clone + 'static {
+    where
+        S: Serialize + std::fmt::Debug + DeserializeOwned + Send + Sync + Clone + 'static,
+    {
         let mut client = Self::get_pool()?;
         let value = serde_json::to_string(&value)?;
 
-        client.hset(key, field, value.clone())?;
-        client.publish(key, value)?;
+        let _: () = client.hset(key, field, value.clone())?;
+        let _: () = client.publish(key, value)?;
 
         Ok(())
     }
@@ -77,7 +83,7 @@ impl CnctdRedis {
             true => {
                 println!("Connected to Redis");
                 Ok(())
-            },
+            }
             false => Err(anyhow::anyhow!("Failed to connect to Redis")),
         }
     }
